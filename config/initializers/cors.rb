ad2e56da6fde744dab53a7c5164eeb3484e7f2ad
Kish-Siva-1 <rodes88@msn.com>
2019-10-25 20:08:46 +0200

    enable cors from anywhere

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'https://firesale-client.herokuapp.com/']
+    origins '*'
+    # ['localhost:3000', 'https://firesale-client.herokuapp.com/']
 
     resource '*',
       headers: :any,
