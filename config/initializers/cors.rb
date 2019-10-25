71effd3a82cb10d5307dfd3cd2d64081486deb0e
Kish-Siva-1 <rodes88@msn.com>
2019-10-25 20:57:15 +0200

    will allow api to communicate with client

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    # ['localhost:3000', 'https://firesale-client.herokuapp.com/']
+    origins ['localhost:3000', 'https://firesale-client.herokuapp.com/']
 
     resource '*',
       headers: :any,
